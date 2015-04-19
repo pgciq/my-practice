9c2ac90368f599895b5c5ab0a0a186f8378311ae
Gary Pan <gary_pan@epam.com>
2015-04-19 15:25:11 +0800

    fetch trunk

--- src/main/java/LearnBigDecimal.java
+++ src/main/java/LearnBigDecimal.java
@@ -48,8 +48,7 @@ public class LearnBigDecimal {
         num.setMinimumFractionDigits(5);
         System.out.println("Formated  PI value : "
                 + num.format(c));
-        String versionControl = "svn used by hk";
-
+        String versionControl = "svn used by hk, git used by sz, use svn commit";
     }
 
 }
